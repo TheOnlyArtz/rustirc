778a4bea4e123a050bf300054fdacf9a5fad2067
amit <gamesil456@gmail.com>
2021-01-13 17:16:31 +0200

    Version 1 to git

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,82 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use async_trait::async_trait;
+use std::error::Error;
+use std::sync::Arc;
+use tokio::io::BufReader;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
+
+// use tokio::net::tcp::{ReadHalf, WriteHalf};
+pub struct Client {
+    ip: String,
+    port: u16,
+    event_handler: Option<Arc<dyn EventHandler>>,
+    pub stream: Option<IrcStream<TcpStream>>
+}
+
+pub enum ClientState {
+    Connecting,
+    Registering,
+    InChannel
+}
+
+impl Client {
+    pub fn new(ip: &str, port: u16) -> Client {
+        Client {
+            ip: ip.to_string(),
+            port,
+            event_handler: None,
+            stream: None
+        }
+    }
+
+    pub fn handler<H: EventHandler + 'static>(mut self, event_handler: H) -> Self {
+        self.event_handler = Some(Arc::new(event_handler));
+        self
+    }
+
+    pub async fn connect(&mut self) -> Result<(), Box<dyn Error>> {
+        self.stream = Some(IrcStream::connect(&self.ip, self.port).await?);
+        Ok(())
+    }
+}
+
+#[async_trait]
+pub trait EventHandler: Send + Sync {
+    async fn test(&self) {}
+}
+
+async fn dispatch(event_handler: &Option<Arc<dyn EventHandler>>) {
+    match event_handler {
+        Some(eh) => eh.test().await,
+        None => {}
+    }
+}
+
+pub struct IrcStream<S> {
+    reader: BufReader<S>
+}
+
+impl IrcStream<TcpStream> {
+    pub async fn connect(peer: &str, port: u16) -> Result<Self, Box<dyn Error>> {
+        let connection = TcpStream::connect(&format!("{}:{}", peer, port)).await?;
+        Ok(IrcStream::new(connection))
+    }
+}
+
+impl<S: AsyncRead + AsyncWrite + Unpin> IrcStream<S> {
+    pub fn new(stream: S) -> Self {
+        IrcStream { reader: BufReader::new(stream) }
+    }
+
+    pub async fn consume_message(&mut self) -> Result<(Vec<u8>, usize), Box<dyn Error>> {
+        let mut buf = Vec::new();
+        let test = self.reader.read_until(b'\n', &mut buf).await?;
+        
+        Ok((buf, test))
+    }
+
+    pub async fn write_all(&mut self, test: &[u8]) -> Result<(), Box<dyn Error>> {
+        self.reader.write(test).await?;
+        Ok(())
     }
 }
---
+++ src/message_parser.rs
@@ -0,0 +1,157 @@
+pub struct Message {
+    pub source: Option<String>,
+    pub command: String,
+    pub parameters: Vec<String>,
+}
+
+pub enum ParsingStage {
+    Beggining,
+    Tags,
+    Source,
+    Command,
+    Parameters,
+}
+
+pub enum ParameterParsingStage {
+    String,
+    General,
+}
+
+pub fn parse_message(raw: &[u8]) -> Result<Message, &'static str> {
+    let mut source: String = "".to_string();
+    let mut command: String = "".to_string();
+    let mut raw_parameters = "".to_string();
+    let mut parsing_stage = ParsingStage::Beggining;
+    let vectorized_raw = raw.to_vec();
+    let mut peekable_raw = vectorized_raw.iter().peekable();
+
+    println!("=====");
+    while peekable_raw.peek().is_some() {
+        let current_char = peekable_raw.next().unwrap();
+        match *current_char as char {
+            ':' => match parsing_stage {
+                ParsingStage::Beggining => {
+                    parsing_stage = ParsingStage::Source;
+                    let (s, p) = consume_attribute(source, peekable_raw, false);
+                    source = s;
+                    peekable_raw = p;
+                }
+                _ => {}
+            },
+            _ => match parsing_stage {
+                ParsingStage::Beggining | ParsingStage::Source | ParsingStage::Tags => {
+                    parsing_stage = ParsingStage::Command;
+                    command += &(*current_char as char).to_string();
+                    let (c, p) = consume_attribute(command, peekable_raw, false);
+                    command = c;
+                    peekable_raw = p;
+                }
+                ParsingStage::Command => {
+                    parsing_stage = ParsingStage::Parameters;
+                    raw_parameters += &(*current_char as char).to_string();
+                    let (pa, p) = consume_attribute(raw_parameters, peekable_raw, true);
+                    raw_parameters = pa;
+                    peekable_raw = p;
+                }
+                _ => {}
+            },
+        }
+    }
+
+    let parameters: Vec<String> = parse_parameters(raw_parameters);
+    println!(
+        "Source: {}\nCommand: {}\nParameters: {:?}\n",
+        source, command, parameters
+    );
+
+    let message = Message {
+        source: Some(source),
+        command,
+        parameters,
+    };
+
+    Ok(message)
+}
+
+pub fn consume_attribute(
+    mut source: String,
+    mut peekable_raw: std::iter::Peekable<std::slice::Iter<u8>>,
+    all: bool,
+) -> (String, std::iter::Peekable<std::slice::Iter<u8>>) {
+    if !all {
+        while peekable_raw.peek().is_some() && (**peekable_raw.peek().unwrap() as char) != ':' {
+            let current_char = peekable_raw.next().unwrap();
+            match *current_char as char {
+                ' ' => {
+                    break;
+                }
+                _ => {
+                    source += &(*current_char as char).to_string();
+                }
+            }
+        }
+    } else {
+        while peekable_raw.peek().is_some() {
+            let current_char = peekable_raw.next().unwrap();
+            source += &(*current_char as char).to_string();
+        }
+    }
+    (source, peekable_raw)
+}
+
+pub fn parse_parameters(parameters: String) -> Vec<String> {
+    let bytes_parameters = parameters.into_bytes();
+    let peekable_parameters = bytes_parameters.iter();
+    let mut peekable_parameters = peekable_parameters.peekable();
+    // let mut stage = ParameterParsingStage::General;
+    let mut parameters: Vec<String> = Vec::<String>::new();
+    while peekable_parameters.peek().is_some() {
+        let current_char = peekable_parameters.next().unwrap();
+        match *current_char as char {
+            ' '|'\r'|'\n' => continue,
+            ':' => {
+                let (s, p) = consume_string(peekable_parameters, false);
+                peekable_parameters = p;
+                parameters.push(s);
+            }
+            _ => {
+                let (mut s, p) = consume_string(peekable_parameters, true);
+                s = format!("{}{}", &(*current_char as char).to_string(), s);
+                peekable_parameters = p;
+                parameters.push(s);
+            }
+        }
+    }
+
+    parameters
+}
+
+pub fn consume_string(
+    mut peekable_raw: std::iter::Peekable<std::slice::Iter<u8>>,
+    stop_on_space: bool,
+) -> (String, std::iter::Peekable<std::slice::Iter<u8>>) {
+    let mut string = String::from("");
+    while peekable_raw.peek().is_some() && (**peekable_raw.peek().unwrap() as char) != ':' {
+        let current_char = peekable_raw.next().unwrap();
+        if stop_on_space {
+            match *current_char as char {
+                ' '|'\r' => {
+                    break;
+                }
+                _ => {
+                    string += &(*current_char as char).to_string();
+                }
+            }
+        } else {
+            match *current_char as char {
+                '\r' => {
+                    break;
+                }
+                _ => {
+                    string += &(*current_char as char).to_string();
+                }
+            }
+        }
+    }
+    (string, peekable_raw)
+}
