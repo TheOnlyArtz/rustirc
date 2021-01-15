6d33dbefb637948d2d9c0ca4ac23ec7951529224
amit <gamesil456@gmail.com>
2021-01-15 22:53:18 +0200

    unused params

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -4,9 +4,9 @@ use super::message_parser::Message;
 
 #[async_trait]
 pub trait EventHandler: Send + Sync {
-    async fn on_message_sent(&self, client: &mut Client, params: Message) {}
-    async fn on_server_connect(&self, client: &mut Client, params: Message) {}
-    async fn on_channel_join(&self, client: &mut Client, params: Message) {}
-    async fn on_message_of_the_day(&self, client: &mut Client, params: Message) {}
-    async fn on_unimplemented(&self, client: &mut Client, params: Message) {}
+    async fn on_message_sent(&self, _client: &mut Client, _params: Message) {}
+    async fn on_server_connect(&self, _client: &mut Client, _params: Message) {}
+    async fn on_channel_join(&self, _client: &mut Client, _params: Message) {}
+    async fn on_message_of_the_day(&self, _client: &mut Client, _params: Message) {}
+    async fn on_unimplemented(&self, _client: &mut Client, _params: Message) {}
 }
